a5e852fc5dad0733697e8dc8eb73e72faeaa7397
BZJ-HUE <2370496566@qq.com>
2026-04-16 14:20:43 +0800

    Update __init__.py

--- MICA-main/__init__.py
+++ MICA-main/__init__.py
@@ -1,14 +1,57 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
-# Author: Yahui Long
-# File Name: __init__.py
-# Description:
+MICA
+====
+
+A Cross-Modal Multi-View Contrastive Learning Framework for Spatial Multi-Omics Integration.
+
+This package provides:
+- model training interface
+- encoder architecture
+- preprocessing functions
+- clustering and visualization utilities
 """
 
-__author__ = "Yahui Long"
-__email__ = "yahuilong1990@gmail.com"
+__title__ = "MICA"
+__version__ = "1.0.0"
+__description__ = "A Cross-Modal Multi-View Contrastive Learning Framework for Spatial Multi-Omics Integration"
 
 from .MICA_pyG import Train_MICA
 from .model import Encoder_overall
-from .preprocess import adjacent_matrix_preprocessing, fix_seed, clr_normalize_each_cell, lsi, construct_neighbor_graph, pca
-from .utils import clustering, plot_weight_value
+from .preprocess import (
+    adjacent_matrix_preprocessing,
+    clr_normalize_each_cell,
+    construct_neighbor_graph,
+    cross_modal_feature_fusion,
+    fix_seed,
+    lsi,
+    pca,
+)
+from .utils import (
+    clustering,
+    evaluate_fusion_effect,
+    mclust_R,
+    plot_weight_value,
+    search_res,
+    visualize_fusion_comparison,
+)
+
+__all__ = [
+    "Train_MICA",
+    "Encoder_overall",
+    "adjacent_matrix_preprocessing",
+    "clr_normalize_each_cell",
+    "construct_neighbor_graph",
+    "cross_modal_feature_fusion",
+    "fix_seed",
+    "lsi",
+    "pca",
+    "clustering",
+    "evaluate_fusion_effect",
+    "mclust_R",
+    "plot_weight_value",
+    "search_res",
+    "visualize_fusion_comparison",
+]
